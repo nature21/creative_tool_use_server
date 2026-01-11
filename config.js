083394383bfdb18bd62b1ecca67d68c8f031bad6
Yuyao Liu <liuyuyao21@mails.tsinghua.edu.cn>
2026-01-11 19:16:14 +0800

    config js updated to google sheets

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
 
 window.APP_CONFIG = {
   // Google Apps Script Web App URL (ends with /exec)
-  GAS_URL: "PASTE_YOUR_APPS_SCRIPT_WEBAPP_URL_HERE",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbz7pPXsPZtcPBkWAG1puD5jz2RPAV8RYK3IhyWWL2T8oA5yNJ_ON43VG7gWGC55-fO5Pw/exec",
 
   // Prolific completion URL, e.g.
   // https://app.prolific.com/submissions/complete?cc=XXXXXX
   // If you prefer, you can omit this and pass ?cc=XXXXXX in the study link.
-  PROLIFIC_COMPLETION_URL: "PASTE_YOUR_PROLIFIC_COMPLETION_URL_HERE",
+  PROLIFIC_COMPLETION_URL: "https://app.prolific.com/submissions/complete?cc=C19DWP4Q",
 
   // Labels shown for the five options
   OPTION_LABELS: ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
